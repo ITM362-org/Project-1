c063df03b06911d5a23595b21c46df7646d6976c
lukas <lproffen@hawk.iit.edu>
2020-02-27 11:45:24 -0600

    adding background image, js alert function

--- script.js
+++ script.js
@@ -1,4 +1,18 @@
 function init(){
 	var button = document.getElementById('submit');
+	
+	function Alert(){
+		var firstname = document.forms["signupform"]["firstname_field"].value;
+		var lastname = document.forms["signupform"]["lastname_field"].value;
+		var email = document.forms["signupform"]["email_field"].value;
+		var language = document.forms["signupform"]["language_field"].value;
+		
+		if (firstname == "")
+		{
+			alert("Enter a first name");
+			return false;
+		}
+	}
+	button.addEventListener('click', Alert);
 }
-
+window.addEventListener('load',init);
